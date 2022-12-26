841a2e88ad29eaed2ae5f75acfbe94815f667841
TEMIMO <artemymif@gmail.com>
2022-12-26 15:25:34 +0300

    Mediator pattern added

--- src/com/temimo/behavioral/mediator/RunMe.java
+++ src/com/temimo/behavioral/mediator/RunMe.java
@@ -2,6 +2,10 @@ package com.temimo.behavioral.mediator;
 
 public class RunMe {
     public static void main(String[] args) {
+
+        // Посредник представляет собой объект, который представляет собой способ взаимодействия множества объектов
+        // между собой и слабой связи между ними. В данном кейсе посредник есть simpleTextChat, а объекты - юзеры и админ
+
         SimpleTextChat simpleTextChat = new SimpleTextChat();
         User admin = new Admin(simpleTextChat, "Admin");
         User user1 = new SimpleUser(simpleTextChat, "User1");
