b318fad49c779e30c4ea5a98eadf58889310f062
TEMIMO <artemymif@gmail.com>
2022-12-27 21:24:19 +0300

    Strategy pattern added

---
+++ src/com/temimo/behavioral/strategy/RunMe.java
@@ -0,0 +1,19 @@
+package com.temimo.behavioral.strategy;
+
+public class RunMe {
+    public static void main(String[] args) {
+        Developer developer = new Developer();
+
+        developer.setActivity(new Sleeping());
+        developer.executeActivity();
+
+        developer.setActivity(new Training());
+        developer.executeActivity();
+
+        developer.setActivity(new Coding());
+        developer.executeActivity();
+
+        developer.setActivity(new Reading());
+        developer.executeActivity();
+    }
+}
---
+++ src/com/temimo/behavioral/strategy/Training.java
@@ -0,0 +1,8 @@
+package com.temimo.behavioral.strategy;
+
+public class Training implements Activity{
+    @Override
+    public void justDoIt() {
+        System.out.println("Training...");
+    }
+}
